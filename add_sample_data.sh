9b4a4517451380aa1ac609f6fdbc09132807e3c1
Shayne Meyer <shayne.meyer@gmail.com>
2023-03-18 06:32:08 -0700

    initial commit

---
+++ add_sample_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for email in $(cat sample_data.txt); do
+  curl -X POST \
+    'localhost:8080/email/create' \
+    --header 'Content-Type: application/json' \
+    --data-raw '{"Email":"'$email'"}'
+done
